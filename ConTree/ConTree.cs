60de0184e312bfe87d1fd62b52c6d9ca7627eb13
kasobo <GitHub.com/kasobo>
2016-10-21 21:09:46 -0700

    updated assembly info

---
+++ ConTree/ConTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace AppMain
+{
+    class ConTreeMain
+    {
+        static int Main (string[] args)
+        {
+            return 0;
+        }
+    }
+}
--- ConTree/Properties/AssemblyInfo.cs
+++ ConTree/Properties/AssemblyInfo.cs
@@ -1,36 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle ("ConTree")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("ConTree")]
-[assembly: AssemblyCopyright ("Copyright ©  2016")]
+[assembly: AssemblyCopyright ("Copyright © kosobo 2016")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible (false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("46c60402-596a-49eb-a398-3acd88a27ab6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: Guid ("1d144f8f-0696-4cfc-a077-620e3b4cb7ee")]
+[assembly: AssemblyVersion ("0.0.1.0")]
