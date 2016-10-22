472dfff34d92424bf70f8b7b0826175f1fcc60f4
kasobo <GitHub.com/kasobo>
2016-10-22 16:51:23 -0700

    Test

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Test")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("tree2")]
+[assembly: AssemblyCopyright ("Copyright ©  2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("0db58f78-b030-463e-97b4-d2d8febb8e4a")]
+[assembly: AssemblyVersion ("0.0.1.0")]
---
+++ Test/TestDirScanner.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using FullSys;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestConCommand
+{
+    [TestClass]
+    public class TestDirTree
+    {
+        public string Root1Name { get { return @"Targets\D1"; } }
+
+        [TestMethod]
+        public void TestStack1()
+        {
+            var d1 = @"Targets\D1";
+            var di1 = new DirectoryInfo (d1);
+
+            var loc = new DirLocation.Nodes (di1);
+            Assert.AreEqual (-1, loc.Depth);
+            loc.Advance();
+            Assert.AreEqual (0, loc.Depth);
+            loc.Advance();
+            Assert.AreEqual (1, loc.Depth);
+        }
+
+        [TestMethod]
+        public void TestScan1()
+        {
+            bool hasDirNope = false;
+            bool hasDirD1E1 = false, hasEmpty = false;
+            var d1 = @"Targets\D1";
+            foreach (DirLocation.Nodes loc in new DirScanner (new DirectoryInfo (d1)))
+            {
+                string dn = loc.Top.Name;
+                hasDirNope |= dn == "NonexistentDir";
+                hasDirD1E1 |= dn == "E1";
+                hasEmpty |= dn == "E5";
+            }
+
+            Assert.IsFalse (hasDirNope);
+            Assert.IsTrue (hasDirD1E1);
+            Assert.IsTrue (hasEmpty);
+        }
+    }
+}
